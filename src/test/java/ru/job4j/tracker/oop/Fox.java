8aeabf394d00263e3cc6c13e4e9ba123597fa03b
Казис <yuka95@inbox.ru>
2022-01-29 19:45:15 +1000

    1.6. Взаимодействие объектов

--- src/test/java/ru/job4j/tracker/oop/Fox.java
+++ src/test/java/ru/job4j/tracker/oop/Fox.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker.oop;
 
 public class Fox {
     public void tryEat(Ball ball) {
-        Ball one = new Ball();
-        one.tryRun(true);
+        ball.tryRun(true);
     }
 }
--- src/test/java/ru/job4j/tracker/oop/Hare.java
+++ src/test/java/ru/job4j/tracker/oop/Hare.java
@@ -2,7 +2,6 @@ package ru.job4j.tracker.oop;
 
 public class Hare {
     public void tryEat(Ball ball) {
-        Ball one = new Ball();
-        one.tryRun(false);
+        ball.tryRun(false);
     }
 }
