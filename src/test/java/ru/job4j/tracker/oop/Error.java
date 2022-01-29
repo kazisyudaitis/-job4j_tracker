e04333cdaf07df5df6758e89015c701f2eb9c369
Казис <yuka95@inbox.ru>
2022-01-29 20:34:55 +1000

    1.7. Конструктор

---
+++ src/test/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    private boolean active;
+
+    private int status;
+
+   private String message;
+
+   public Error(boolean active, int status, String message) {
+       this.active = active;
+       this.status = status;
+       this.message = message;
+   }
+
+   public Error() {
+   }
+
+   public void printInfo() {
+       System.out.println("Активность: " + active);
+       System.out.println("Статус: " + status);
+       System.out.println("Сообщение: " + message);
+   }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 5, "Есть ошибки!");
+        error.printInfo();
+        Error err = new Error(true, 3, "Еще остались ошибки!");
+        err.printInfo();
+        Error noErr = new Error();
+        noErr.printInfo();
+    }
+}
