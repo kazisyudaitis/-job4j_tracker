75a3a427c5d087e9361daed0ee824e25623815a5
Казис <yuka95@inbox.ru>
2022-02-12 22:04:41 +1000

    9. toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,5 +9,7 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String createDate = formatter.format(item.getCreated());
         System.out.println(createDate);
+        Item item1 = new Item();
+        System.out.println(item1.toString());
     }
 }
