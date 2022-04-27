1a970377288220c7973c23bda9b690108940932f
Казис <yuka95@inbox.ru>
2022-04-27 19:36:02 +1000

    0. Set, HashSet, Iterator

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,12 @@
+package ru.job4j.collection;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+
+        return rsl;
+    }
+
+}
