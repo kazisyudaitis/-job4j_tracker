a8e1fbd2eff2375977e817d2ae7001b6176f5466
Казис <yuka95@inbox.ru>
2022-05-22 19:33:55 +1000

    2. Комбинированный компаратор

--- src/main/java/ru/job4j/collection/JobDescByNameLn.java
+++ src/main/java/ru/job4j/collection/JobDescByNameLn.java
@@ -10,16 +10,12 @@ public class JobDescByNameLn implements Comparator<Job> {
 
     public static void main(String[] args) {
         List<Job> jobs = Arrays.asList(
-               /* new Job("Fix bug", 1),
+                new Job("Fix bug", 1),
                 new Job("Fix bug", 4),
                 new Job("Fix bug", 2),
                 new Job("X task", 0)
         );
-        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));*/
-                new Job("Impl task", 0),
-                new Job("Impl task", 1)
-        );
-                Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
+        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
     }
 }
